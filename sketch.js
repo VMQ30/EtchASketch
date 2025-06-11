8f3fd8b18615cddbfe3cc787e0d7674cc9ed9764
Vida <quiniavidamarie30@gmail.com>
2025-06-11 14:58:42 +0800

    made css changes and added logic for adding the grid

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let grid = document.getElementById(`grid`);
+
+let sizeOfGrid = 16;
+let size = sizeOfGrid*sizeOfGrid;
+
+let squareSize = 100/sizeOfGrid;
+
+for(let i = 0; i<size;i++){
+    let div = document.createElement('div');
+    grid.appendChild(div);
+    div.setAttribute("style", "`width:${squareSize}`%; height:${squareSize};");
+}
